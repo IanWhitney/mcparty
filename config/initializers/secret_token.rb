2838ffc17484a8984c14ec600ad81466af063e6b
Ian Whitney <ian@ianwhitney.com>
2012-09-29 20:45:36 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcparty::Application.config.secret_token = '9b9a26fcb8d8e0897357f08eb8d09e8456104cf5db0b0ba8d31716d5a42dfaad21e9ece7f7128e1fd77e2257ab9e17ff5b72d5da8669b8325ee94699d95e93b4'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Mcparty::Application.routes.draw do
+  root to: "main#index"
+end
